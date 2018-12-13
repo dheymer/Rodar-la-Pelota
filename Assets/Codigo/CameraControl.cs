6148b313c18123d2823c15523d177d2b3ad6f98b
Dheymer León <dheymer@gmail.com>
2018-12-13 14:09:40 -0500

    Added files to repository

---
+++ Assets/Codigo/CameraControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+    public GameObject objPlayer;
+    private Vector3 relativePosition;
+
+	// Use this for initialization
+	void Start () {
+        relativePosition = transform.position - objPlayer.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = objPlayer.transform.position + relativePosition;
+	}
+}
---
+++ Assets/Codigo/PlayerControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour
+{
+    int intContador;
+    public Text txtScore, txtMessageWin;
+    Rigidbody rb;
+    public float velocidad;
+
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        intContador = 0;
+        ScoreUpdate();
+        txtMessageWin.gameObject.SetActive(false);
+    }
+
+    private void ScoreUpdate()
+    {
+        txtScore.text = "Puntuación: " + intContador;
+    }
+
+    public void FixedUpdate()
+    {
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+        Vector3 movimiento = new Vector3(movimientoHorizontal, 0, movimientoVertical);
+        rb.AddForce(movimiento * velocidad);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Colisión");
+        Destroy(other.gameObject);
+        intContador = intContador + 1;
+        ScoreUpdate();
+        if (intContador >= 12) {
+            txtMessageWin.gameObject.SetActive(true);
+        }
+    }
+}
